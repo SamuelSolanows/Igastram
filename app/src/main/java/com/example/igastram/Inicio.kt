7b7187a1c2ba899b7d830d49a149bbc4454b4188
ASUS <samuelsolanoama@gmail.com>
2023-07-27 19:18:19 -0500

    Fragmentos

--- app/src/main/java/com/example/igastram/Inicio.kt
+++ app/src/main/java/com/example/igastram/Inicio.kt
@@ -2,13 +2,30 @@ package com.example.igastram
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
 import com.example.igastram.databinding.ActivityInicioBinding
 
 class Inicio : AppCompatActivity() {
     lateinit var binding: ActivityInicioBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding= ActivityInicioBinding.inflate(layoutInflater)
+        binding = ActivityInicioBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.apply {
+            button.setOnClickListener {
+                LlamarFragmento(savedInstanceState)
+            }
+        }
+
+    }
+
+    private fun LlamarFragmento(savedInstanceState: Bundle?) {
+        if (savedInstanceState == null) {
+            supportFragmentManager.commit {
+                setReorderingAllowed(true)
+                add<Notificaciones>(R.id.fragmentContainerView2)
+            }
+        }
     }
 }
